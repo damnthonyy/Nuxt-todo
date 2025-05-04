1713370ab019027b6d0cd60609f87c4d05b913ea
damnthonyy <mahassadijoseph@gmail.com>
2025-05-04 19:45:44 +0200

    feat: add TodoItem component to dashboard

---
+++ app/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flex justify-center mt-10">
+        <div class="flex gap-2">
+            <input type="text" v-model="newTodo" @keyup.enter="saveTodo" class="border-2 w-[350px] h-[40px] rounded-md p-2"/>
+            <button type="button" @click="saveTodo" class="bg-blue-500 text-white p-2 rounded-md cursor-pointer w-[100px]">Save</button>
+            <button type="button" @click="deleteTodo" class="bg-red-500 text-white p-2 rounded-md cursor-pointer w-[100px]">Delete</button>
+        </div>
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// Definir TodoItem
+
+interface TodoItem{
+    id?: number;
+    title?: string;
+    completed?: boolean;
+}
+
+
+const props = defineProps<TodoItem>()
+
+// Definir newTodo
+const newTodo = ref<string>('');
+
+// Callbacks Save, Delete,
+
+function saveTodo(): void {
+    // code
+    console.log(newTodo.value);
+}
+
+function deleteTodo(): void {
+    //code 
+    console.log(`${newTodo.value} deleted`);
+}
+
+</script>
+
+
--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -4,5 +4,6 @@
 
 <template>
     <h1 class="text-2xl font-bold text-center">dashboard</h1>
+    <TodoItem/>
   
 </template>
